2ed660ffc3c553b6f31a3f166fc50a89b328ad1d
Ansh Garewal <anshgarewal7@gmail.com>
2023-06-24 01:29:33 +0530

    Pdf manager

---
+++ src/main/java/com/pdfmanager/pdfmanager/PdfManagerApplication.java
@@ -0,0 +1,18 @@
+package com.pdfmanager.pdfmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfManagerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PdfManagerApplication.class, args
+
+
+
+		);
+	}
+
+}
---
+++ src/test/java/com/pdfmanager/pdfmanager/PdfManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdfmanager.pdfmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
