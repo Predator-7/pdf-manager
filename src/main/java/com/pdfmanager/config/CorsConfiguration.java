4aabd5d4c123b0369d040c4db96ff9a7f7f1b630
Predator-7 <anshgarewal7@gmail.com>
2023-06-24 18:37:56 +0530

    CORS changes

---
+++ src/main/java/com/pdfmanager/config/CorsConfiguration.java
@@ -0,0 +1,14 @@
+package com.pdfmanager.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/pdf-manager/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
